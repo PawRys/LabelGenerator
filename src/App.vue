2ee45b2dd5a93968f8f555bd403ef13f2ef81164
PawRys <hellbu@gmail.com>
2023-05-30 17:50:52 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+function onFileUpload(event) {
+	console.log(event.target.files);
+}
+</script>
+
+<template>
+	<h1>Etykieter</h1>
+
+	<input type="file" id="file-upload" @change="onFileUpload" multiple />
+</template>
+
+<style scoped></style>
