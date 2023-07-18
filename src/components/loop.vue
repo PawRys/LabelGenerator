e666eb2093382c447b91cf9ec5a86eee3ab0013e
PawRys <hellbu@gmail.com>
2023-07-18 20:33:34 +0200

    First commit

---
+++ src/components/loop.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {} from 'vue';
+const props = defineProps({
+	item: Array,
+});
+
+const item = props.item;
+</script>
+
+<template>
+	<li v-for="n of +item[4]">
+		<slot></slot>
+	</li>
+</template>
