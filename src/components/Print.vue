2d47903b969517aa08dc7ece81ec8e0defcf49b6
PawRys-Smetek <hellbu@gmail.com>
2024-10-30 11:25:52 +0100

    print css

--- src/components/Print.vue
+++ src/components/Print.vue
@@ -31,88 +31,94 @@ const labelsStore = useLabelsStore();
 </template>
 
 <style scoped>
-.page {
-	display: grid;
-	gap: 1cm;
-	grid-template-rows: 1fr 1px 1fr;
-	height: calc(100vh - 1mm);
-	height: calc(100svh - 1mm);
-	page-break-before: always;
-	overflow: hidden;
-}
-.page:nth-of-type(1) {
-	page-break-before: avoid;
-}
+@media print {
+	.page {
+		width: 100vw;
+		height: 100vh;
+		max-width: 100%;
+		max-height: 100%;
+		box-sizing: border-box;
+		overflow: clip;
+		margin: 0;
+		padding: 0;
 
-.label-divider {
-	border-bottom: dashed 1px gray;
-	width: 100%;
-}
+		display: grid;
+		gap: 1cm;
+		grid-template-rows: 1fr 1px 1fr;
+		page-break-before: always;
+	}
 
-.label {
-	/* box-shadow: 0 0 1px red; */
-	display: grid;
-	gap: 0.5cm;
-	align-self: self-end;
-	align-items: baseline;
-	grid-template-columns: auto 1fr auto;
-	grid-template-rows: 1fr auto 1fr;
-	grid-template-areas:
-		'A A A'
-		'B B B'
-		'C D E';
+	.page:nth-of-type(1) {
+		page-break-before: avoid;
+	}
 
-	/* height: 100%; */
-	max-height: calc((100vh - 1mm) / 2);
-	/* max-height: calc((100svh - 1mm) / 2); */
-	overflow: clip;
+	.label-divider {
+		border-bottom: dashed 1px gray;
+		width: 100%;
+	}
+	.label {
+		display: grid;
+		gap: 0.5cm;
+		align-self: self-end;
+		align-items: baseline;
+		grid-template-columns: auto 1fr auto;
+		grid-template-rows: 1fr auto 1fr;
+		grid-template-areas:
+			'A A A'
+			'B B B'
+			'C D E';
 
-	font-size: 3cm; /* Master font size for all label childs */
-	--fs-normal: 3cm;
-	--fs-small: calc(var(--fs-normal) * 0.75);
-	--fs-smaller: calc(var(--fs-normal) * 0.55);
-	--fs-smallest: calc(var(--fs-normal) * 0.35);
-	font-weight: 500;
-	text-align: center;
-	line-height: 1;
-}
+		max-height: calc(50vh - 0.5cm - 1px);
+		overflow: clip;
 
-.label__item-size {
-	grid-area: A;
-	font-size: var(--fs-normal);
-	font-weight: 600;
-}
-.label__long-desc {
-	grid-area: B;
-	font-size: var(--fs-small);
-	white-space: pre-line;
-	text-align: center;
-	min-height: 1em;
-	max-height: 60%;
-	/* height: 20%; */
-	/* height: min-content; */
-	overflow: clip;
-	/* font-size: 12px; */
-}
+		font-size: 3cm; /* Master font size for all label childs */
+		--fs-normal: 3cm;
+		--fs-small: calc(var(--fs-normal) * 0.75);
+		--fs-smaller: calc(var(--fs-normal) * 0.55);
+		--fs-smallest: calc(var(--fs-normal) * 0.35);
+		font-weight: 500;
+		text-align: center;
+		line-height: 1;
+	}
 
-.label__item-glue {
-	grid-area: C;
-	font-size: var(--fs-smaller);
-}
+	.label__item-size {
+		grid-area: A;
+		font-size: var(--fs-normal);
+		font-weight: 600;
+	}
+	.label__long-desc {
+		grid-area: B;
+		font-size: var(--fs-small);
+		white-space: pre-line;
+		text-align: center;
+		min-height: 1em;
+		/* height: min-content; */
+		overflow: hidden;
+	}
 
-.label__contract {
-	grid-area: D;
-	font-size: var(--fs-smallest);
-	font-weight: 400;
-}
-.label__pack-size {
-	grid-area: E;
-	font-size: var(--fs-normal);
-	font-weight: 600;
-}
-.label__pack-size::after {
-	content: 'szt.';
-	font-size: var(--fs-smallest);
-	font-weight: 400;
+	.label__item-glue {
+		grid-area: C;
+		font-size: var(--fs-smaller);
+	}
+
+	.label__contract {
+		grid-area: D;
+		font-size: var(--fs-smallest);
+		font-weight: 400;
+	}
+	.label__pack-size {
+		grid-area: E;
+		font-size: var(--fs-normal);
+		font-weight: 600;
+	}
+	.label__pack-size::after {
+		content: 'szt.';
+		font-size: var(--fs-smallest);
+		font-weight: 400;
+	}
+
+	@page {
+		margin: 0;
+	}
 }
 </style>
