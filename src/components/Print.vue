ce8ec2141814e444d19fbc7edd0db9edd53aa3cf
PawRys-Smetek <hellbu@gmail.com>
2024-11-25 11:31:45 +0100

    css

--- src/components/Print.vue
+++ src/components/Print.vue
@@ -59,6 +59,17 @@ const labelsStore = useLabelsStore();
 		gap: 1cm;
 		grid-template-rows: 1fr 1px 1fr;
 		page-break-before: always;
+
+		--fs-normal: 2.7cm;
+		--fs-small: calc(var(--fs-normal) * 0.75);
+		--fs-smaller: calc(var(--fs-normal) * 0.55);
+		--fs-smallest: calc(var(--fs-normal) * 0.35);
+
+		font-family: 'Roboto Flex', serif;
+		font-optical-sizing: auto;
+		font-style: normal;
+		font-variation-settings: 'slnt' 0, 'wdth' 100, 'GRAD' 0, 'XOPQ' 96, 'XTRA' 468, 'YOPQ' 79, 'YTAS' 750, 'YTDE' -203,
+			'YTFI' 738, 'YTLC' 514, 'YTUC' 712;
 	}
 
 	.page:nth-of-type(1) {
